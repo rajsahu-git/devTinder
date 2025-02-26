db4f35500229695bc996d739e0f502ad5f0e8291
Raj Sahu <55652441+rajsahu-git@users.noreply.github.com>
2025-02-26 21:44:03 +0530

    create multipal routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,29 @@ const express = require('express')
 
 const app = express()
 
-app.use("/",(req,res)=>{
-    res.send("heelo from server")
+app.get("/user/:id",
+    (req,res,next)=>{
+    console.log(req.params)
+    res.send(req.params)  
+    next()
+},
+(req,res,next)=>{
+    res.send("kuch nhi")
+    next()
+})
+app.post("/user",(req,res)=>{
+    res.send("successfully send data")
 })
 
+app.put("/user",(req,res)=>{
+    res.send("update data")
+})
+
+app.patch("/user",(req,res)=>{
+    res.send("data has been patch")
+})
+
+app.delete("/user",(req,res)=>{
+    res.send("user has been delete")
+} )
 app.listen(3000,()=>console.log("server start"))
